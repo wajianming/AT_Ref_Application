86794a35dd77a877595b175e8d3df2dba9acee95
JM <r91921051@ntu.edu.tw>
2016-06-19 23:59:23 +0800

    Revert "boot thread practice"
    This reverts commit 3cfaaaf6948e8d118fa21be51345ed8ad6d87a50.

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -1,14 +1,6 @@
 #include "HelloLib.h"
-
-#include "thread.h"
-#include <iostream>
 #include <iostream>
 
-int main()
-{
-    RESOURCE_MONITOR::resource_observer observer;
-    while (true){
-        sleep(1);
-    }
-    exit(0);
+int main(){
+    HelloLib lib;
 }
